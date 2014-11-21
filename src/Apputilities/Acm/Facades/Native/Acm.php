fb458421fd24bea4d94db37cfb28b9b40efe8b19
krossroad <krossroad07@gmail.com>
2014-11-21 14:46:47 +0545

    Added Native Facades for Acm

--- src/Apputilities/Acm/Facades/Native/Acm.php
+++ src/Apputilities/Acm/Facades/Native/Acm.php
@@ -1,12 +1,34 @@
 <?php
 
-namespace Apputilities\Acm\Facdes\Native;
+namespace Apputilities\Acm\Facades\Native;
+
+use Apputilities\Acm\AcmSentry;
 
 /**
  * @author Rikesh <rikesh.shrestha.npl@gmail.com>
  **/
 class Acm
 {
+    protected static $acmSentry;
 
+    public static function __callStatic($name, $args)
+    {
+        self::boot();
+
+        if (method_exists(self::$acmSentry, $name)) {
+            return self::$acmSentry->$name($args);
+
+        } else {
+            throw new \Exception('Method you are looking for doesn\'t exists.');
+        }
+
+    }
+
+    public function boot()
+    {
+        if (is_null(self::$acmSentry)) {
+            self::$acmSentry = new AcmSentry();
+        }
+    }
 }
 // END class Acm
